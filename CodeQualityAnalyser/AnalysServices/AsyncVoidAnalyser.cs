9d1438214cf62160b981572c4085571e03676a5e
semenovkiryha <semenovkiryha@gmail.com>
2026-07-08 22:15:29 +0300

    AsyncVoidAnalyser

---
+++ CodeQualityAnalyser/AnalysServices/AsyncVoidAnalyser.cs
@@ -0,0 +1,28 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeQualityAnalyser;
+
+public class AsyncVoidAnalyser : IAnalyser
+{
+    public List<string> GetAnalysis(SyntaxTree tree)
+    {
+        var errors = new List<string>();
+        var root = tree.GetRoot();
+
+        foreach (var method in root.DescendantNodes().OfType<MethodDeclarationSyntax>())
+        {
+            var isAsync = method.Modifiers.Any(SyntaxKind.AsyncKeyword);
+            var isVoid = method.ReturnType is PredefinedTypeSyntax predefinedType &&
+                         predefinedType.Keyword.IsKind(SyntaxKind.VoidKeyword);
+
+            if (isAsync && isVoid)
+            {
+                errors.Add($"[AsyncError] Метод '{method.Identifier.Text}' объявлен как async void! Асинхронные методы должны возвращать Task или Task<T>, иначе упадет все приложение.");
+            }
+        }
+
+        return errors;
+    }
+}
