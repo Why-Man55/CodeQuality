9aec7bc71f5d470cd1e860db78e2a78b313323fa
semenovkiryha <semenovkiryha@gmail.com>
2026-07-08 22:46:37 +0300

    added filename in errror message

--- CodeQualityAnalyser/AnalysServices/EmptyCatchAnalyser.cs
+++ CodeQualityAnalyser/AnalysServices/EmptyCatchAnalyser.cs
@@ -1,5 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
 
 namespace CodeQualityAnalyser;
 
@@ -8,13 +9,14 @@ public class EmptyCatchAnalyser : IAnalyser
     public List<string> GetAnalysis(SyntaxTree tree)
     {
         var errors = new List<string>();
+        var fileName = Path.GetFileName(tree.FilePath);
         var root = tree.GetRoot();
 
         foreach (var catchClause in root.DescendantNodes().OfType<CatchClauseSyntax>())
         {
             if (!catchClause.Block.Statements.Any())
             {
-                errors.Add("[AntiPattern] Обнаружен пустой блок catch! Нельзя молча подавлять ошибки. Добавь логирование или 'throw;'.");
+                errors.Add($"[AntiPattern] {fileName}: Обнаружен пустой блок catch! Нельзя молча подавлять ошибки. Добавь логирование или 'throw;'.");
             }
         }
 
